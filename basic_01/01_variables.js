a42abc93959cf556ec91989091be12116bd63a92
SAURABH SHARMA <144321708+saurabhshar966@users.noreply.github.com>
2026-03-27 19:29:39 +0000

    DATATYPE

---
+++ basic_01/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 123132
+let accountEmail = "saurabhsh855@gmail.com"
+var accountPassward ="23443"
+accountCity ="Jaipur"   // we declear a memory without variable use //
+
+let accountState;
+
+
+// accountId = 3 // not allowed becouse use const variable
+
+accountEmail="saur@email.com"
+accountPassward ="756566"
+accountCity="barabanki"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassward,accountCity , accountState])
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scoped
+
+*/
---
+++ basic_01/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version 
+
+// alert("hello") // we are usingn node js , not browser
+
+console.log(6
+    
+    +
+    6) // code readability should be high 
+
+    console.log("saurabh")
+
+    let name = "hitesh"
+    let age = 38
+    let isLoggedIn = false
+
+    //number => to power 53
+    // bigint
+    // string => "  any noun"
+    // boolean => true or false 
+    //null=? standlone value 
+    // undefined => 
+    // symbol=> uniqe
+    //Object 
+    
+    console.log(typeof "hitesh");
+    console.log(typeof undefined); // undefined 
+    console.log(typeof null); // object -----> intresting part upcoming lectures 
