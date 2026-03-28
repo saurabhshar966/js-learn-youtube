cdb47be30002d2ff5245538256fda2f5362ef293
SAURABH SHARMA <144321708+saurabhshar966@users.noreply.github.com>
2026-03-28 07:42:44 +0000

    datatype

---
+++ basic_01/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// let score = "33xyz"
+
+// let score = null      /*--> give ouput 0 */
+
+// let score = undefined
+
+// let score = true 
+let score = false
+
+
+//console.log(typeof score);
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" ==> 33
+// "33xyz"==> NaN
+// ture => 1; false => 0 
+
+let isLoggedIn = "Prince"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+
+// 1=> ture; => false
+//"" ==> false
+//"hitesh" ==> true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
