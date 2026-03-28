4d7b0d60ed30092249b56fda718344119df84363
SAURABH SHARMA <144321708+saurabhshar966@users.noreply.github.com>
2026-03-28 08:29:19 +0000

    conversion

--- basic_01/03_conversionOperation.js
+++ basic_01/03_conversionOperation.js
@@ -34,8 +34,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//*****************OPERATIONS *******************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+//basic math operations--------------------------->>>
+
+let str1 = "hello"
+let str2 = " saurabh"
+let str3 = str1+str2
+/*
+console.log(str3);
+
+
+console.log("2" + 2);
+console.log(2 + "2");
+console.log("2"+ 2 + 3);
+console.log(2 + 2 +  "3");
+
+*/
+
+
+//console.log(+true);
+//console.loh(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+
+++gameCounter; 
+console.log(gameCounter);
+
 
 
 
