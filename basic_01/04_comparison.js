75b2e59f87d502480d0c548f2d7bb6d72d479e8a
SAURABH SHARMA <144321708+saurabhshar966@users.noreply.github.com>
2026-03-30 06:23:40 +0000

    comparision datatype using operator

---
+++ basic_01/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log (2 > 1);
+// console.log ( 2 >= 1);
+// console.log ( 2 > 1);
+// console.log ( 2 == 1);
+// console.log ( 2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   
+
+//NOTE ::----->> //When you use comparison operators like >=, 
+// JavaScript converts null to a number.null becomes 0 //  
+// and avoide this type of conversion (give importance to clean code)
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === stict check by teiple equal check 
+
+console.log("2" === 2);
+
+
+
+
+
