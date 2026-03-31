98a90aec7d6d20349ead746babc6ebd5d95ef975
SAURABH SHARMA <144321708+saurabhshar966@users.noreply.github.com>
2026-03-31 18:47:04 +0000

    stack and heap concept

--- basic_01/04_comparison.js
+++ basic_01/04_comparison.js
@@ -16,13 +16,38 @@ console.log(null >= 0);
 // JavaScript converts null to a number.null becomes 0 //  
 // and avoide this type of conversion (give importance to clean code)
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
 // === stict check by teiple equal check 
 
-console.log("2" === 2);
+// console.log("2" === 2);
+
+
+//**********************MEMORY********************************** */
+
+
+//stack(Primitive), heap(Non-Primitive) 
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@goole.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email ="hitesh@google.com"
+console.log(userOne.email)
+console.log(userTwo.email);
 
 
 
